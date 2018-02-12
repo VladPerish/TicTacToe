eb65725a8bc5ce5ab20c4c26e8fdeb689e6379e3
VladPerish <perish.abs@gmail.com>
2018-02-12 09:25:51 +0600

    score not settled :D

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,22 @@
-//JS code goes here
+//JS code goes
+// var restart = document.querySelector('#b');
+//
+// var squares = document.querySelectorAll("td")
+// for (var i = 0; i < squares.length; i++) {
+//     squares[i].textContent = '';
+// }
+//
+// }
+// restart.addEventListener('click',clearBoard)
+var turn = false;
+function play (event) {
+	if(!event.target.innerText) {
+		event.target.innerText = turn? "O": "X";
+		turn = !turn;
+	}
+}
+
+var gameOver= false;
+function checkBoard(game over) {
+
+}
